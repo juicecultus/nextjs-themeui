dc704117444f831092331ac804aa68e72df73568
juicecultus <juicecultus@gmail.com>
2020-08-05 20:26:51 +0100

    add plugins using next-config

---
+++ next-config.js
@@ -0,0 +1,32 @@
+const nextEnv = require('next-env');
+const dotenvLoad = require('dotenv-load');
+
+dotenvLoad();
+
+const withNextEnv = nextEnv();
+module.exports = withNextEnv();
+
+// module.exports = {
+//   webpack: {
+//     // webpack config properties
+//   },
+//   env: {
+//     MY_ENV_VAR: process.env.SECRET,
+//   },
+// };
+
+// const { PHASE_PRODUCTION_SERVER } = require('next/constants')
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
+// module.exports = (phase, { defaultConfig }) => {
+//   if (phase === PHASE_PRODUCTION_SERVER) {
+//     return {
+//       ...defaultConfig,
+//       webpack: {
+//         plugins: [new BundleAnalyzerPlugin()]
+//       }
+//     }
+//   }
+
+//   return defaultConfig
+// }
