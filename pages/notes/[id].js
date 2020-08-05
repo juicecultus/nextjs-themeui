ea9bd4a5e8590c86b4b03457e42a8157bc41d9be
juicecultus <juicecultus@gmail.com>
2020-08-05 19:59:13 +0100

    basic nextJS app themed with theme-ui

---
+++ pages/notes/[id].js
@@ -0,0 +1,17 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+
+const DynamicPage = () => {
+  const router = useRouter();
+  const { id } = router.query;
+
+  return (
+    <div sx={{ variant: 'containers.page' }}>
+      <h1>Note: {id} </h1>
+    </div>
+  );
+};
+
+export default DynamicPage;
